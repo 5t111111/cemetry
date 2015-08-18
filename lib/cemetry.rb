2f68ad62576c0c58d353f544002376bba226ae82
Hirofumi Wakasugi <baenej@gmail.com>
2015-08-18 11:37:36 +0900

    Implement core features

--- lib/cemetry.rb
+++ lib/cemetry.rb
@@ -1,5 +1,26 @@
 require "cemetry/version"
+require "miserable"
+require "osc-ruby"
 
 module Cemetry
-  # Your code goes here...
+  class Sequence
+    attr_reader :bpm_msec
+
+    def initialize(bpm)
+      @bpm_msec = Miserable.new(bpm)
+    end
+
+    def play(proc)
+      begin
+        osc_client = OSC::Client.new("localhost", 2345)
+      rescue SocketError => e
+        puts e.message
+        retry
+      end
+      loop do
+        osc_client.send(OSC::Message.new("/sequence", proc.call))
+        sleep(@bpm_msec.quarter_note / 1_000)
+      end
+    end
+  end
 end
